d4cb4cba77697c310a147b6ba9cb9ddce1dfc11a
lLucasl <m1939128298@outlook.com>
2026-06-30 16:10:24 +0800

    删除 build

--- build/lvgl_build/CMakeFiles/lvgl.dir/src/libs/fsdrv/lv_fs_cbfs.c.o.d
+++
@@ -1,3 +0,0 @@
-lvgl_build/CMakeFiles/lvgl.dir/src/libs/fsdrv/lv_fs_cbfs.c.o: \
- /Users/shingbd/workspace/qiansai/3rdparty/lvgl/src/libs/fsdrv/lv_fs_cbfs.c \
- /usr/include/stdc-predef.h
--- build/lvgl_build/CMakeFiles/lvgl.dir/src/misc/lv_templ.c.o.d
+++
@@ -1,3 +0,0 @@
-lvgl_build/CMakeFiles/lvgl.dir/src/misc/lv_templ.c.o: \
- /Users/shingbd/workspace/qiansai/3rdparty/lvgl/src/misc/lv_templ.c \
- /usr/include/stdc-predef.h
